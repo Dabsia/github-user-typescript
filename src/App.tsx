b7d271cf964147f562280a708b2a6a08babb305f
Dabsia <Dabojohnson98@gmail.com>
2022-12-31 00:37:04 -0800

    Finished the app

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export interface Iuser {
   login: string;
   id: number;
   node_id: string;
-  avatar_url: string;
+  avatar_url: 'https://i.picsum.photos/id/210/536/354.jpg?hmac=qPCpI_y93j9pbBfCzroTfdmomnJbuPBHJp_r4Qxoh2s';
   gravatar_id: string;
   url: string;
   html_url: string;
@@ -16,20 +16,20 @@ export interface Iuser {
   gists_url: string;
   starred_url: string;
   subscriptions_url: string;
-  organizations_url: string;
+  organizations_url: null;
   repos_url: string;
   events_url: string;
   received_events_url: string;
   type: string;
   site_admin: boolean;
   name: string;
-  company?: null;
+  company?: string;
   blog: string;
   location: string;
   email?: null;
   hireable?: null;
   bio: string;
-  twitter_username?: null;
+  twitter_username?: string;
   public_repos: number;
   public_gists: number;
   followers: number;
@@ -41,12 +41,51 @@ export interface Iuser {
 
 const App: React.FC = () => {
 
-  const [userData, setUserData] = useState<Iuser>()
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const [userData, setUserData] = useState<Iuser>({
+  login: '',
+  id: 0,
+  node_id: '',
+  avatar_url: 'https://i.picsum.photos/id/210/536/354.jpg?hmac=qPCpI_y93j9pbBfCzroTfdmomnJbuPBHJp_r4Qxoh2s',
+  gravatar_id: '',
+  url: '',
+  html_url: '',
+  followers_url: '',
+  following_url: '',
+  gists_url: '',
+  starred_url: '',
+  subscriptions_url: '',
+  organizations_url: null,
+  repos_url: '',
+  events_url: '',
+  received_events_url: '',
+  type: '',
+  site_admin: false,
+  name: '',
+  company: 'Not Available',
+  blog: 'Not Available',
+  location: 'Not Available',
+  email: null,
+  hireable: null,
+  bio: '',
+  twitter_username: 'Not Available',
+  public_repos: 0,
+  public_gists: 0,
+  followers: 0,
+  following: 0,
+  created_at: '',
+  updated_at: ''
+  })
 
   const userNameHandler = (userInput: string): void => {
+    setIsLoading(true)
     fetch(`https://api.github.com/users/${userInput}`)
       .then(res => res.json())
-      .then(data => setUserData(data))
+      .then(data => {
+        setUserData(data)
+        setIsLoading(false)
+       })
 
   }
 
@@ -57,6 +96,7 @@ const App: React.FC = () => {
     <div className='app'>
       <div className='appItems'>
         <SearchBox userNameHandler={userNameHandler} />
+        {isLoading && <p style={{'color': '#fff'}}>Loading...</p>}
         <Details userData={userData} />
       </div>
     </div>
--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,69 +1,79 @@
-import {useState} from 'react'
 import './Details.css'
-import Image from '../../Assets/Dabo Johnson.jpg'
 import { Iuser } from '../../App'
 
 
 const Details: React.FC<{userData: Iuser}> = ({ userData }): JSX.Element => {
 
-    // const [Username, setUsername] = useState<string>('Username')
-    
-    console.log(userData.bio)
+  const createdDate = new Date(userData.created_at)
 
-    // if (userData.bio === 0) {
-        
-    // }
-    
+  const newDate = createdDate.toDateString()
+
+  if (userData.twitter_username === null) {
+    userData.twitter_username = 'Not Available'
+  }
+
+  if (userData.blog === null) {
+    userData.blog = 'Not Available'
+  }
+
+  if (userData.location === null) {
+    userData.location = 'Not Available'
+  }
+  if (userData.company === null) {
+    userData.company = 'Not Available'
+  }
+
+  
   return (
       <div className='DetailsBox'>
           <div className='left'>
-              <img className='image' src = {Image} alt = 'avatar' />
+              <img className='image' src = {userData.avatar_url} alt = 'avatar' />
           </div>
           <div className='right'>
               <div className='userNameDetails'>
                 <div>
-                    <p>Username</p>
-                    <p>FullName</p>
-                    <p>This user has no bio</p>
+                    <p>{userData.login}</p>
+                    <p>{userData.name}</p>
+                    <p>{userData.bio}</p>
                 </div>
-                <p>Joined: 2019-12-26</p>
+                <p>Joined: {newDate}</p>
               </div>
               <div className='activities'>
                   <div className='actions' >
                       <p>Repos</p>
-                      <p>88</p>
+                      <p>{userData.public_repos}</p>
                   </div>
                   <div className='actions' >
                       <p>Followers</p>
-                      <p>7</p>
+                      <p>{userData.followers}</p>
                   </div>
                   <div className='actions' >
                       <p>Following</p>
-                      <p>9</p>
+                      <p>{userData.following}</p>
                   </div>
               </div>
               <div className='socialsContainer'>
                   
                 <div className='socials'>
                     <i className="ri-map-pin-2-line newIcon"></i>
-                    <span>Not Available</span>
+                    <span>{userData.location}</span>
                   </div>
                   
                   <div className='socials'>
                     <i className="ri-twitter-fill newIcon"></i>
-                    <span>Not Available</span>
+                    <span>{userData.twitter_username}</span>
                 </div>
               </div>
               <div className='socialsContainer'>
                   
                 <div className='socials'>
                     <i className="ri-links-line newIcon"></i>
-                    <span>Not Available</span>
+                    <span>{userData.blog}</span>
                   </div>
                   
                   <div className='socials'>
                     <i className="ri-building-fill newIcon"></i>
-                    <span>Not Available</span>
+                    <span>{userData.company}</span>
                 </div>
               </div>
           </div>
